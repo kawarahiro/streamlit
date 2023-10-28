62ddd8d2bc583a15b9e9ecd967ff5a778b91d22b
kawarahiro <kawara.hiro@gmail.com>
2023-10-28 12:40:32 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import time
+
+st.title('Streamlit 超入門')
+
+'Start!!'
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'Done!!!!'
+
+text = st.text_input('あなたの趣味を教えてください。')
+condision = st.slider('あなたの今の調子は？', 0, 100, 50)
+
+'あなたの趣味：', text
+'コンディション：', condision
+
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラム')
+
+expander = st.expander('問い合わせ')
+expander.write('問い合わせ内容を書く')
+
+
